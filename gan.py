def342e84f152f97be9dbd0d1dd32680143a7cf3
Surya Teja Menta <mentasuryateja@gmail.com>
2022-03-04 13:32:49 +0530

    Slug balence set 10

--- gan.py
+++ gan.py
@@ -13,22 +13,7 @@ class gan:
         try:
             """Inference demo for Real-ESRGAN.
             """
-            parser = argparse.ArgumentParser()
-            parser.add_argument('-i', '--input', type=str, default='inputs', help='Input image or folder')
-            parser.add_argument('-n','--model_name',type=str,default='RealESRGAN_x4plus',help=('Model names: RealESRGAN_x4plus | RealESRNet_x4plus | RealESRGAN_x4plus_anime_6B | RealESRGAN_x2plus'
-                    'RealESRGANv2-anime-xsx2 | RealESRGANv2-animevideo-xsx2-nousm | RealESRGANv2-animevideo-xsx2'
-                    'RealESRGANv2-anime-xsx4 | RealESRGANv2-animevideo-xsx4-nousm | RealESRGANv2-animevideo-xsx4'))
-            parser.add_argument('-o', '--output', type=str, default='results', help='Output folder')
-            parser.add_argument('-s', '--outscale', type=float, default=4, help='The final upsampling scale of the image')
-            parser.add_argument('--suffix', type=str, default='out', help='Suffix of the restored image')
-            parser.add_argument('-t', '--tile', type=int, default=0, help='Tile size, 0 for no tile during testing')
-            parser.add_argument('--tile_pad', type=int, default=10, help='Tile padding')
-            parser.add_argument('--pre_pad', type=int, default=0, help='Pre padding size at each border')
-            parser.add_argument('--face_enhance', action='store_true', help='Use GFPGAN to enhance face')
-            parser.add_argument('--half', action='store_true', help='Use half precision during inference')
-            parser.add_argument('--alpha_upsampler',type=str,default='realesrgan',help='The upsampler for the alpha channels. Options: realesrgan | bicubic')
-            parser.add_argument('--ext',type=str,default='auto',help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs')
-            args = parser.parse_args()
+            
 
             # determine models according to model names
             
